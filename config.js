1b78deb8de7c105a12ad97a215cabe62b7ae9810
Sown <114833586+sown-discord@users.noreply.github.com>
2023-12-03 18:15:09 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
     "1095592017121251400"
   ],
   "color": "#2300ff",
-  "panel": "51.255.215.122",
+  "panel": "",
   "port": "30140",
   "version": "v0.0.1",
   "versionapi": "V1.3.5",
